4b054e5eddd98de6a37e444645f1653ca721d028
kriquero <75072125+kriquero@users.noreply.github.com>
2025-06-14 23:52:38 +0200

    Primer golpe

---
+++ JS/menuWritter.js
@@ -0,0 +1,8 @@
+document.getElementById('menuContainer').innerHTML =
+'<div class="menu_container"><a href="C:/Users/Kike/Desktop/vampiro V20/Mexico Maldito/Localizacioens/Listado_Localizaciones.html"> ' +
+'   <div class="menu_element">Lugares</div></a><a href="C:/Users/Kike/Desktop/vampiro V20/Mexico Maldito/NPC/Listado_Personajes.html">   ' +
+'    <div class="menu_element">Personajes</div></a><a href="C:/Users/Kike/Desktop/vampiro V20/Mexico Maldito/Jugadores/Listado_Jugadores.html">  ' +
+'      <div class="menu_element">Jugadores</div></a><a href="C:/Users/Kike/Desktop/vampiro V20/Mexico Maldito/Organizaciones/Listado_organizaciones.html">  ' +
+'        <div class="menu_element">Organizaciones</div></a><a href="C:/Users/Kike/Desktop/vampiro V20/Mexico Maldito/Tramas/Listado_Sesiones.html">  ' +
+'          <div class="menu_element">Tramas</div></a></div> <a href="C:/Users/Kike/Desktop/vampiro V20/Mexico Maldito/Disciplinas/Disciplinas.html">' +
+'          <div class="menu_element">Disciplinas</div></a><a href="C:/Users/Kike/Desktop/vampiro V20/Mexico Maldito/Musica/Musica.html"><div class="menu_element">Múscia</div></a>';
